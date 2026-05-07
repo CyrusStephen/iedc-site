e390fe70ee627d0aef0883ad789be9d3144e406a
CyrusStephen <95368747+CyrusStephen@users.noreply.github.com>
2026-05-07 23:02:46 +0530

    removed useless pages

--- src/app/join/page.js
+++
@@ -1,9 +0,0 @@
-import JoinSection from "../../components/Join.js";
-
-export default function JoinPage() {
-  return (
-    <main className="bg-black text-white min-h-screen flex items-center justify-center px-4">
-      <JoinSection />
-    </main>
-  );
-}
